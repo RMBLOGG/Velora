fd13cc9c04d009dab1b23d3b306b2a29115549d5
RMBLOGG <ramdanhidayah176@gmail.com>
2026-06-11 03:16:20 +0700

    Fix workflow like Aniku

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
   namespace = "com.velora"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.velora.app"
     minSdk = 24
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
 
